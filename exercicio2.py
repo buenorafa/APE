83ba1a3f15511c048f859394966876d57b7f96ac
buenorafa <rafaelbuenooo@outlook.com>
2022-08-25 12:30:34 -0300

    Exercicio de APE - IFPB

---
+++ exercicio2.py
@@ -0,0 +1,39 @@
+# 1: A Companhia de Carros Usados João Honesto paga seus empregados um salário de R$
+# 1.000,00 por mês mais uma comissão de R$ 200,00 por cada carro vendido mais 5% do
+# valor da venda.
+# Escreva um programa que leia o nome, o número de carros vendidos e o valor total das
+# vendas de um vendedor, e calcule e exiba o seu salário.
+
+# nome = input('Digite o nome do vendedor: ')
+# carros = int(input('Digite o numero de carros vendidos: '))
+# vendas = float(input('Digite o valor total das vendas: '))
+# comissao =  carros * 200
+# salario = 1000 + comissao + (vendas * 0.05)
+# print(f'O salario de {nome} foi R$ {salario:.2f}')
+
+# 2: Escreva um programa para calcular e exibir a média ponderada de 2 notas dadas. Sabe-
+# se que nota1 possui peso 6 e nota2 possui peso 4.
+
+# nota1 =  float(input('Digite a nota 1: '))
+# nota2 =  float(input('Digite a nota 2: '))
+# media = ((nota1 * 6) + (nota2 * 4))/ 10
+# print(f'A media ponderada eh: {media}')
+
+# 3: Escreva um programa que leia duas variáveis inteiras e troque o conteúdo entre elas,
+# mostrando o resultado.
+
+# var1 =  int(input('Digite a primeira variavel inteira: '))
+# var2 =  int(input('Digite a segunda variavel inteira: '))
+# aux = var1
+# var1 = var2
+# var2 = aux
+# print(f'var1: {var1} e var2: {var2}')
+
+# 4: Um motorista anota a marcação do hodômetro do seu veículo antes e após uma viagem,
+# bem como o número de litros de combustível gastos.
+# Escreva um programa que leia os 3 dados acima, o preço do litro de combustível, a
+# capacidade do tanque e mostre:
+# a) Quilometragem rodada.
+# b) Quantos quilômetros por litro faz o veículo.
+# c) Autonomia do veículo.
+# d) Custo da viagem.
---
+++ questao1.py
@@ -0,0 +1,26 @@
+# #Exe 1: PRINT
+# print("OLA MUNDO")
+# #Exe 2: DOBRO
+# numero = int(input('Digite um numero inteiro: '))
+# resultado = numero * 2
+# print(f'O dobro de {numero} eh {resultado}')
+# #Exe 3: SOMA DE DOIS REAIS
+# num1 = float(input('Digite o primeiro numero:'))
+# num2 = float(input('Digite o segundo numero:'))
+# print(f'A soma deles eh: {num1 + num2}')
+# #Ex4: AREA RETANGULO
+# base = float(input('Digite a base: '))
+# altura = float(input('Digite a altura: '))
+# area = (base*altura)/2
+# print(f'A area eh: {area}')
+# #Ex5: Nome e Sobrenome
+# nome = input('Digite o nome: ')
+# sobrenome = input('Digite o sobrenome: ')
+# print(f'{sobrenome}, {nome}')
+# #Ex6: Cotcao dolar
+# dolar = float(input('Digite a cotacao do dolar: '))
+# quant_dolar = float(input('Digite a quantidade de dolar: '))
+# print(f'O valor em reais é: {dolar * quant_dolar}')
+#Ex7: Restaurante peso
+valor_do_prato =  float(input('Digite o valor do prato em kg: '))
+print(f'O valor total é {valor_do_prato*25:.2f}')
